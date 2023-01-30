62597eb822cfe4b183db283f10a5fbe6d5a70225
ounis houcine <ounishoucine.on@gmail.com>
2023-01-30 01:57:01 +0100

    add script  CARRE

---
+++ creation geometrique-cercle.py
@@ -0,0 +1,24 @@
+# Programme Python pour dessiner
+# les cercles tangents
+# Import du module turtle
+import turtle
+#Initialisation de l’instance turtle
+tur = turtle.Turtle()
+# rayon du plus petit cercle
+rayon = 10
+# nombre des cercles
+nb = 10
+# boucle pour dessiner les cercles tangents
+for i in range(1, nb + 1, 1):
+   tur.circle(rayon*i)
+
+   # Utilisation d’une boucle pour dessiner un carré avec turtle
+   # On importe le module turtle
+import turtle
+
+# Initialisation du dessinateur
+tur = turtle.Turtle()
+# Début de la boucle
+for i in range(4):  # la boucle va tourner 4 fois
+   tur.forward(100)  # Avance le dessinateur de 100 pas en avant
+tur.left(90)  # Rotation du dessinateur de 90 degrés
